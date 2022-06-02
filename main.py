082ab93c1ae7bb2aca79169403d0ae01a2c40db6
MoaidF <moaid@foodwize.co>
2022-06-02 15:02:08 +0200

    Adding error showing with original message for error handling

--- main.py
+++ main.py
@@ -13,28 +13,28 @@ Out[4]: [1659, 8963]"""
 try:
     Lock=Locker(1659, 9123)
     UHF=UHF_Reader(1659,8963)
-except Exception:
-    print("Please unplug the devices")
+except BaseException as e :
+    print(str(e))
 #####################################Initilize Parameters########################################
 Opened_Lock=False
 try:
     API.POST_inventory([UHF.Get_Inventory()])
     Change_status = API.PUT_status("CLOSED")
-except Exception:
-    print(Exception)
+except BaseException as e :
+    print(str(e))
 ###################################################################################################
 while(1):
     try:
         response_status = API.Get_status()
-    except Exception :
-        print(Exception)
+    except BaseException as e :
+        print(str(e))
     #################################Opening the Canteen Procedure#########################################
     try:
         if (Lock.is_open()==False)and(response_status.json()["canteenStatus"]=='OPEN')and(Opened_Lock==False):
             Opened_Lock=True
             Lock.Open_L()    
-    except Exception :
-            print(Exception)
+    except BaseException as e :
+        print(str(e))
     #################################Closing the Canteen Procedure#########################################
     try:
 
@@ -42,5 +42,5 @@ while(1):
             Change_status = API.PUT_status("CLOSED")
             API.POST_inventory([UHF.Get_Inventory()])
             Opened_Lock=False
-    except Exception :
-            print(Exception)
+    except BaseException as e :
+        print(str(e))
